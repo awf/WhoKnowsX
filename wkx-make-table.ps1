e959c02ddacd86cdcb925c6c1fb05d45aeeb976b
Andrew Fitzgibbon <awf@microsoft.com>
2018-10-24 18:30:59 +0100

    Much better language handling

--- wkx-make-table.ps1
+++ wkx-make-table.ps1
@@ -1,18 +1,50 @@
-param($noload = $false)
-$languages = echo js cs hs c cpp m java py sh rb bat go ts R vb fs hs ps1 jl ocaml
+param($csv = "wkx.csv", $load = $true)
 
-if (!$noload) {
-  write-host "loading"
-  $csv = Import-Csv C:\tmp\github-popularity.csv
+$languages = (cat .\languages.json |ConvertFrom-Json).languages
+$languages|%{ 
+    $ext2lang = @{} 
+  } { 
+    $name = $_.name; 
+    $exts = $_.extensions
+    $exts |% { 
+      if ($_){
+        $ext2lang[$_] += $name +"/"
+      }
+    }
+    if (!$exts) {
+      write-warning "No exts for [$_]"
+    }
+  }
+
+$keys = @() + $ext2lang.keys
+foreach ($ext in $keys) {
+  $ext2lang[$ext] = $ext2lang[$ext] -replace '/$'," [$ext]"
 }
 
-write-host "processing"
-$csv | % {
-  $_.file = $_.file -replace '^.*\.','' ; $_
-} | group file | % {
-  $ext = $_.name;
-  if ($ext -in $languages) {
+if ($load) {
+  write-host "wkx-make-table: loading $csv"
+  $data = Import-Csv $csv
+}
+
+write-host "wkx-make-table: processing"
+# overwrite "file" field in place to language
+$data | % {
+  $ext = $_.file -replace '^.*\.','.'
+  $lang = $ext2lang[$ext]
+  if (!$lang) { 
+    #write-warning "wkx-make-table: Unknown extension [$ext]"
+    #$lang = $ext
+    #$ext2lang[$ext] = $ext -replace '^.',''
+  }
+  $_.file = $lang
+}
+
+$data | group file | % {
+  $lang = $_.name;
+  if (!$lang) { 
+    #write-warning "wkx-make-table: Empty lang! [$_]"
+  } else {
     $authors = $_.group | group author;
-    awf-new @{ext=$ext;n_authors=$authors.count;authors=$authors.name}
+    ./awf-new @{lang=$lang;n_authors=$authors.count;authors=$authors.name}
   }
 } | sort n_authors
